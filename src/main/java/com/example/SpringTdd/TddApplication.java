4505ed22578a678de95df8b557cda81c67899c27
victor oliveira <vimatozin@hotmail.com>
2023-03-21 07:30:02 -0300

    Fixed repository query and added servlet initializer

--- src/main/java/com/example/SpringTdd/TddApplication.java
+++ src/main/java/com/example/SpringTdd/TddApplication.java
@@ -1,13 +1,39 @@
 package com.example.SpringTdd;
 
+import com.example.SpringTdd.model.BookingModel;
+import com.example.SpringTdd.repository.BookingRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TddApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TddApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TddApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initdatabase(BookingRepository bookingRepository) {
+        return args -> {
+            bookingRepository.deleteAll();
+            BookingModel reserve = new BookingModel();
+            LocalDate checkIn = LocalDate.parse("2022-11-10");
+            LocalDate checkOut = LocalDate.parse("2022-11-20");
+            reserve.setId("1");
+            reserve.setCheckIn(checkIn);
+            reserve.setCheckOut(checkOut);
+            reserve.setNumberGuests("2");
+            reserve.setReserveName("Victor e Carolyne");
+            bookingRepository.save(reserve);
+        };
+
+    }
 
 }
+
+
+
--- src/main/java/com/example/SpringTdd/repository/BookingRepository.java
+++ src/main/java/com/example/SpringTdd/repository/BookingRepository.java
@@ -3,18 +3,16 @@ package com.example.SpringTdd.repository;
 import com.example.SpringTdd.model.BookingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingModel, String> {
     @Transactional(readOnly = true)
-    @Query(" SELECT * FROM booking_model bm WHERE bm.reserve_name = :reserveName ORDER BY bm.reserve_name")
-    Optional<BookingModel> findByReserveName(@Param("reserveName") String reserveName);
+    @Query("select b from BookingModel b where b.reserveName like ?1")
+    Optional<BookingModel> findByReserveName(String reserveName);
 
 }
